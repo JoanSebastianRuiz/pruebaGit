8b701a522fa096e34aa4f859ce92b7efffeb6976
JoanSebastianRuiz <joansebastianruizangarita10@gmail.com>
2024-07-11 16:31:50 -0500

    Revert "feat: :sparkles: Inicio del programa"
    This reverts commit eb3f9d6a1550948942baaccf8cc9f8714f069ce2.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 print("hola, esto es una práctica")
 print("No eliminen nada")
+<<<<<<< HEAD
 print("Sapo")
 print("hola")
 print("ldkjdsalk")
+=======
+print("Sapo")
+>>>>>>> parent of eb3f9d6 (feat: :sparkles: Inicio del programa)
